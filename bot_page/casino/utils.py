f18f9fd132a2958b1f1d5d7a4157b5f424d33bb0
Dogson <dogsonkrul@gmail.com>
2021-06-12 21:16:31 +0200

    check password in bot endpoints

--- bot_page/casino/utils.py
+++ bot_page/casino/utils.py
@@ -1,10 +1,14 @@
+from secrets import compare_digest
 from django.http import JsonResponse
+from django.contrib.auth import settings
+
+SECRET_KEY = settings.SECRET_KEY
 
 
 def check_ip(function):
     def wrapper(request):
-        ip = request.META.get('REMOTE_ADDR')
-        if ip == "127.0.0.1":
+        password = request.POST.get("django_password")
+        if compare_digest(password, SECRET_KEY):
             return function(request)
         else:
             return JsonResponse({"status": "forbidden"})
