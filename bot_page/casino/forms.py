4759594a589379f05abbb33a92f578297ad731e1
Dogson <dogsonkrul@gmail.com>
2021-06-21 19:32:57 +0200

    bet casino form: default percent to win is now 50%

--- bot_page/casino/forms.py
+++ bot_page/casino/forms.py
@@ -4,7 +4,8 @@ from django import forms
 class BetForm(forms.Form):
     percent_to_win = forms.IntegerField(label="""Procent na wygraną: <span class='text-success' id='percent_label'>50%</span><br>
                                                  Nagroda w wyniku wygranej: <span class='text-success' id='multiplier_label'>0</span>""",
-                                        widget=forms.NumberInput(attrs={"class": "form-control mb-2", "type": "range", "min": "1", "max": "90"}),
+                                        widget=forms.NumberInput(attrs={"class": "form-control mb-2",  "type": "range",
+                                                                        "min": "1", "max": "90", "value": 50}),
                                         min_value=1, max_value=90)
     bet_money = forms.FloatField(label="Liczba obstawionych monet",
                                  widget=forms.NumberInput(attrs={"class": "form-control mb-2"}), min_value=0)
