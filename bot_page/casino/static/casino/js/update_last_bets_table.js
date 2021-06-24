9f63e513bf40260d26b4f120a3d49d2121ce76b1
Dogson <dogsonkrul@gmail.com>
2021-06-24 19:58:26 +0200

    casino bets, frontend: fixed bug (if user made less bets than 10, user last_bets table didn`t update)

--- bot_page/casino/static/casino/js/update_last_bets_table.js
+++ bot_page/casino/static/casino/js/update_last_bets_table.js
@@ -1,21 +1,14 @@
-const user_bets_table = document.querySelectorAll("#user_bets_table > * ")
+const user_bets_table = document.getElementById("user_bets_table")
 
 function update_user_last_bets(bet_data, win)
 {
-    let table_data = []
-    for(let col of user_bets_table)
-    {
-        table_data.push(col.innerHTML)
-    }
-    for(let i=1; i<user_bets_table.length-1; i++)
-    {
-        user_bets_table[i].innerHTML = table_data[i-1]
-    }
+    let user_bets_table_children = document.querySelectorAll("#user_bets_table > * ")
+    let first_element = user_bets_table_children[0].cloneNode(true)
 
     let j = 0
-    for(let i of user_bets_table[0].children)
+    for(let i of first_element.children)
     {
-        if(i.children.length===1)
+        if(i.children.length==1)
         {
             let span_class = (win===0) ? "'text-danger'>" : "'text-success'>"
             i.innerHTML = "<span class=" + span_class + bet_data[j] + " </span>"
@@ -26,5 +19,10 @@ function update_user_last_bets(bet_data, win)
         }
         j++
     }
-}
 
+    user_bets_table.insertAdjacentElement("afterbegin", first_element)
+    if(user_bets_table_children.length == 10)
+    {
+        user_bets_table.removeChild(user_bets_table.lastElementChild)
+    }
+}
